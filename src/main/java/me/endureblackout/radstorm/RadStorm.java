839544a51c0988aa4db994f06f31e3da1f877cad
Rayzr522 <rayzr522@gmail.com>
2016-10-04 18:01:20 -0400

    >_<

--- src/main/java/me/endureblackout/radstorm/RadStorm.java
+++ src/main/java/me/endureblackout/radstorm/RadStorm.java
@@ -11,46 +11,52 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class RadStorm extends JavaPlugin {
-	public void onEnable() {
-		
-		new BukkitRunnable() {
-			public void run() {
-				if(CommandHandler.enabled == 0) {
-					for(World world : Bukkit.getServer().getWorlds()) {
-						CommandHandler.enabled = 1;
-						world.setStorm(true);
+    public void onEnable() {
 
-					}
-					Bukkit.getServer().broadcastMessage(ChatColor.RED + "[RS] RadStorm has been initiated. Take cover!");
-				}
-			}
-		}.runTaskTimer(this, this.getConfig().getInt("RadStorm Between")*20, this.getConfig().getInt("RadStorm Between")*20);
-		
-		File file = new File(getDataFolder(), "config.yml");
-		if(!(file.exists())) {
-			try {
-				saveConfig();
-				setupConfig(getConfig());
-				getConfig().options().copyDefaults(true);
-				saveConfig();
-			} catch (Exception e){
-				e.printStackTrace();
-			}
-		}
-		
-		Bukkit.getServer().getPluginManager().registerEvents(new StormHandler(this), this);
-		
-		getCommand("radstorm").setExecutor(new CommandHandler(this));
-		
+        Runnables.init(this);
 
-	}
-	
-	private void setupConfig(FileConfiguration config) throws IOException {
-		if (!new File(getDataFolder(), "RESET.FILE").exists()) {
-			new File(getDataFolder(), "RESET.FILE").createNewFile();
-			getConfig().set("RadStorm Time", 420);
-			getConfig().set("RadStorm Damage", 2.0);
-			getConfig().set("RadStorm Between", 1800);
-		}
-	}
+        new BukkitRunnable() {
+            public void run() {
+                if (CommandHandler.enabled == 0) {
+                    for (World world : Bukkit.getServer().getWorlds()) {
+                        CommandHandler.enabled = 1;
+                        world.setStorm(true);
+
+                    }
+                    Bukkit.getServer().broadcastMessage(ChatColor.RED + "[RS] RadStorm has been initiated. Take cover!");
+                }
+            }
+        }.runTaskTimer(this, this.getConfig().getInt("RadStorm Between") * 20, this.getConfig().getInt("RadStorm Between") * 20);
+
+        File file = new File(getDataFolder(), "config.yml");
+        if (!(file.exists())) {
+            try {
+                saveConfig();
+                setupConfig(getConfig());
+                getConfig().options().copyDefaults(true);
+                saveConfig();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Bukkit.getServer().getPluginManager().registerEvents(new StormHandler(this), this);
+
+        getCommand("radstorm").setExecutor(new CommandHandler(this));
+
+    }
+
+    @Override
+    public void onDisable() {
+        Runnables.deinit();
+    }
+
+    private void setupConfig(FileConfiguration config) throws IOException {
+        if (!new File(getDataFolder(), "RESET.FILE").exists()) {
+            new File(getDataFolder(), "RESET.FILE").createNewFile();
+            getConfig().set("RadStorm Time", 420);
+            getConfig().set("RadStorm Damage", 2.0);
+            getConfig().set("RadStorm Between", 1800);
+        }
+    }
 }
